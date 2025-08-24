ac8b7f4bfb4e891b3cf021cf260e47f8a319e34d
Nayeem Hasan Shanto <snayeemhasan@gmail.com>
2025-08-24 14:09:13 +0600

    dunamically updated text from input

---
+++ styles/js/another.js
@@ -0,0 +1,21 @@
+console.log("File Connected");
+
+/* click here to add button e click krole text area theke input nibe and sheta uporer paragraph section e add korbe. and text area khali kore dibe. */
+
+document.getElementById('click-btn').addEventListener('click', function(){
+    console.log("Button clicked");
+    const textArea = document.getElementById('text-area');
+    console.log(textArea);
+    const inputValue = textArea.value;
+    console.log(inputValue); 
+
+    const newChild = document.createElement('p');
+    /* console.log(newChild); */
+    newChild.innerText = inputValue;
+    /* console.log(newChild); */
+
+    const newElement = document.getElementById('paragraph-container');
+    console.log(newElement);
+    newElement.appendChild(newChild);
+    textArea.value = '';
+})
